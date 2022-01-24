afa4f80af9a981695dfd6b1aefc04cb343f83d66
Nicolas Muller <zepouet@gmail.com>
2022-01-24 19:05:28 +0100

    WIP

--- .scripts/create-sp-prod13114-deploy-webapp.sh
+++ .scripts/create-sp-prod13114-deploy-webapp.sh
@@ -8,8 +8,8 @@ SUBSCRIPTION_ID=54d87296-b91a-47cd-93dd-955bd57b3e9a
 APP_NAME=webprod13114
 REGISTRY=regprod13114
 
-#az ad sp create-for-rbac --name ${SERVICE_PRINCIPAL_NAME} --role contributor \
-#  --scopes /subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${RG_NAME}/providers/Microsoft.Web/sites/${APP_NAME} \
-#  --sdk-auth
+az ad sp create-for-rbac --name ${SERVICE_PRINCIPAL_NAME} --role contributor \
+  --scopes /subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${RG_NAME}/providers/Microsoft.Web/sites/${APP_NAME} \
+  --sdk-auth
 
-az role assignment create --assignee 9988e8bc-8649-4607-b8e5-12f1806d5535 --scope /subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${RG_NAME}/providers/Microsoft.ContainerRegistry/registries/${REGISTRY} --role "AcrPull"
+#az role assignment create --assignee 9988e8bc-8649-4607-b8e5-12f1806d5535 --scope /subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${RG_NAME}/providers/Microsoft.ContainerRegistry/registries/${REGISTRY} --role "AcrPull"
